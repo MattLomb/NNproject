c0db9c8e04d834112503b9fdcdfae18b044353b6
Luca <l.giovannesi@gmail.com>
2021-09-17 17:49:17 +0200

    New code

--- run.py
+++ run.py
@@ -2,7 +2,13 @@ import argparse
 import os
 import numpy as np
 import pickle
+'''
 from pymoo.optimize import minimize
 from pymoo.algorithms.so_genetic_algorithm import GA
 from pymoo.factory import get_algorithm, get_decision_making, get_decomposition
 from pymoo.visualization.scatter import Scatter
+'''
+import models
+from models import StyleGAN2
+config = "kfkdf"
+a=StyleGAN2(config)
