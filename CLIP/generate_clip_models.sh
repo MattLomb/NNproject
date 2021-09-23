07803f70dac1198bca4f10e047628310feb93b42
MatLomb <matlomb97@gmail.com>
2021-09-23 16:26:51 +0200

    Update generate_clip_models file

--- CLIP/generate_clip_models.sh
+++ CLIP/generate_clip_models.sh
@@ -2,4 +2,4 @@ git clone https://github.com/RobertBiehl/CLIP-tf2.git
 conda deactivate #If needed
 cd CLIP-tf2
 pip install -r requirements.txt -U
-python3 convert_clip.py --model "ViT-B/32" --output ../model
+python3 convert_clip.py --model "ViT-B/32" --text_output ../models/text_model --image_output ../models/image_model --output ../models/general
